fc253f387e3f6002f4e9c5c2f5a765ae34069a10
Jakob Schrettenbrunner <dev@schrej.net>
2018-05-08 22:17:07 +0200

    thanks dane

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# website
+# pterodactyl.io
+
+This is the Pterodactyl project website.
+
+## Compilation
+
+We are using [PostCSS](http://postcss.org/) and [Pug](https://pugjs.org/api/getting-started.html) to make creating the website easier. Therefore the website needs to be built before it can be viewed in a browser. To automate that we are using [gulp](https://gulpjs.com/).
+
+You'll need to install the dependencies and `gulp-cli` first.
+```sh
+# install dependencies
+npm i
+# install gulp cli globally
+npm i -g gulp-cli
+```
+
+For development serve the website locally using [Browsersync](https://browsersync.io/).
+```
+gulp serve
+```
+
+To just build the website run
+```
+gulp build
+```
+
+## Dependencies
+This is everything we use on the website, apart from the gulp related tooling.
+
+ * [tailwindcss](https://tailwindcss.com/docs/what-is-tailwind/) - the main css framework
+ * [jQuery](https://jquery.com) - well, jQuey, also slick.js depends on it
+ * [slick.js](https://kenwheeler.github.io/slick/) - the screenshot gallery
+ * [particles.js](https://vincentgarreau.com/particles.js/) - the fancy particles in the header
+ * [elegant themes line](https://www.elegantthemes.com/blog/resources/how-to-use-and-embed-an-icon-font-on-your-website) - the feature icons
+ * [Poppins](https://fonts.google.com/specimen/Poppins) - the font
---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+const gulp = require('gulp')
+const plumber = require('gulp-plumber')
+const browserSync = require('browser-sync').create()
+const clean = require('gulp-clean')
+
+const postcss = require('gulp-postcss')
+const sourcemaps = require('gulp-sourcemaps')
+
+const pug = require('gulp-pug')
+
+const vendor = [
+    'slick-carousel/slick/'
+]
+
+gulp.task('clean', () => {
+    return gulp.src('./build/*')
+        .pipe( clean() )
+})
+
+gulp.task('html', () => {
+    return gulp.src('./src/**/*.pug')
+        .pipe( plumber() )
+        .pipe( pug() )
+        .pipe( gulp.dest('./build/') )
+})
+
+gulp.task('css', () => {
+    return gulp.src('./src/assets/css/main.css')
+        .pipe( plumber() )
+        .pipe( sourcemaps.init() )
+        .pipe( postcss([
+            require('postcss-import'),
+            require('tailwindcss')('./tailwind.js'),
+            require('precss'),
+            require('autoprefixer')
+        ]) )
+        .pipe( sourcemaps.write('.') )
+        .pipe( gulp.dest('./build/assets/css/') )
+        .pipe( browserSync.stream() )
+})
+
+gulp.task('copy', () => {
+    return gulp.src(['./src/assets/**/*.+(png|json|svg)', './src/assets/+(fonts|js)/**', './src/favicon.ico'], { relative: true, base: './src/' })
+        .pipe( plumber() )
+        .pipe( gulp.dest('./build/'))
+})
+
+gulp.task('build', gulp.series('clean', gulp.parallel('copy', 'html', 'css')))
+
+gulp.task('serve', gulp.series(gulp.parallel('copy', 'html', 'css'), () => {
+    browserSync.init({
+        server: "./build"
+    })
+
+    gulp.watch('./src/assets/**/*.png', gulp.parallel('copy'))
+    gulp.watch('./src/assets/css/**/*.css', gulp.parallel('css'))
+    gulp.watch('./src/**/*.pug', gulp.series('html', browserSync.reload))
+}))
